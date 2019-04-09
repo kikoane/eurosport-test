3936d59b51cbe8cfc75dd9f558ca34a06e8f6bda
kikoane <kenguyen@student.42.fr>
2019-04-09 20:30:04 +0200

    Updates

---
+++ precache-manifest.ac52cdcc5ef2115b458fdc1d0ee67092.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "70341de566392670f11f",
+    "url": "/eurosport-test/static/css/main.230165ac.chunk.css"
+  },
+  {
+    "revision": "70341de566392670f11f",
+    "url": "/eurosport-test/static/js/main.dfea3429.chunk.js"
+  },
+  {
+    "revision": "66802e896ba1d58e5ff9",
+    "url": "/eurosport-test/static/js/runtime~main.f17c7beb.js"
+  },
+  {
+    "revision": "bbe04e8e77df7f39fd20",
+    "url": "/eurosport-test/static/js/2.132531d8.chunk.js"
+  },
+  {
+    "revision": "eaa5d63ab108ef7be4cb7e39a964af03",
+    "url": "/eurosport-test/index.html"
+  }
+];
